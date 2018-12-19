01f335fa84e607218f04dbeb3659dfcaed623cbf
guilherme <gui11_3@hotmail.com>
2018-12-19 16:30:46 -0200

    Create Post http

--- services/HttpService.js
+++ services/HttpService.js
@@ -1,12 +1,19 @@
 import fetch from 'isomorphic-unfetch'
 import propostaConfig from '../proposta-credito.config'
 
-
 class HttpService {
-    static async get(path="", body = {'Content-Type': 'application/json'}){
+    static async get(path="", headers = {'Content-Type': 'application/json'},body={}){
+        return fetch(propostaConfig.baseURL+path,{
+            method: 'GET',
+            headers: headers,
+            body: JSON.stringify(body)
+        })
+    }
+
+    static async post(path="", headers = {'Content-Type': 'application/json'},body={}){
         return fetch(propostaConfig.baseURL+path,{
             method: 'POST',
-            headers: body,
+            headers: headers,
             body: JSON.stringify(body)
         })
     }
